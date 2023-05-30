e976ba2d411ccd2bd2b77d2a9ae8ff9a2c8fc59a
simonweigold <simonw750@gmail.com>
2023-05-30 18:48:49 +0200

    provided script which works with kaggle data.

---
+++ scripts/spotify-charts-2022.R
@@ -0,0 +1,89 @@
+# Load packages -----------------------------------------------------------
+library(tidyverse)
+library(rio)
+library(tidyr)
+library(igraph)
+library(here)
+
+
+# Data import -------------------------------------------------------------
+spotify <- import(here::here("data", "charts.csv"))
+#global charts
+global <- spotify %>% filter(region == "Global")
+global <- global %>% filter(trend == 'NEW_ENTRY') %>% filter(chart == 'top200')
+global = global[order(global[,'title'],-global[,'streams']),]
+global = global[!duplicated(global$title),]
+# usa <- spotify %>% filter(region == "United States")
+# usa = usa[order(usa[,'title'],-usa[,'streams']),]
+# usa = usa[!duplicated(usa$title),]
+# germany <- spotify %>% filter(region == "Germany")
+# germany = germany[order(germany[,'title'],-germany[,'streams']),]
+# germany = germany[!duplicated(germany$title),]
+
+
+# Pre-processing ----------------------------------------------------------
+
+
+
+
+
+
+# Residual code -----------------------------------------------------------
+
+# Pre-processing ----------------------------------------------------------
+df <- Spotify %>% select(artist_names)
+df_split <- separate(df, col = artist_names, into = paste0("text", 1:7), sep = ",", extra = "merge")
+df_split <- df_split %>% filter(!is.na(df_split$text2))
+df_test <- df_split %>% select(text1, text2)
+for (col in names(df_split)) {
+  if (is.character(df_split[[col]])) {
+    df_split[[col]] <- trimws(df_split[[col]])
+  }
+}
+for (col in names(df_test)) {
+  if (is.character(df_test[[col]])) {
+    df_test[[col]] <- trimws(df_test[[col]])
+  }
+}
+network <- graph_from_data_frame(df_test, directed = F)
+
+
+# Visualisation of top nodes ----------------------------------------------
+# get the degree of each node
+degree <- degree(network)
+V(network)$degree <- degree
+# sort the degrees in descending order and select the top n indices
+n <- 100
+top_nodes <- order(degree, decreasing = TRUE)[1:n]
+# extract the subgraph consisting of the selected nodes
+subgraph <- induced_subgraph(network, top_nodes)
+# plot the subgraph
+plot(subgraph,
+     vertex.shape = "circle",
+     vertex.size = V(subgraph)$degree,
+     edge.arrow.size = 0.15,
+     vertex.label.cex = 0.8, # label font size
+     vertex.color = "seagreen1",
+     edge.color = "black",
+     vertex.label.color = "black",
+     vertex.label.family = "Helvetica",
+     vertex.label.font = 2, # bold  font
+     )
+
+
+simple_subgraph <- simplify(subgraph)
+sg_degree <- degree(simple_subgraph)
+V(simple_subgraph)$degree <- sg_degree
+communities <- fastgreedy.community(simple_subgraph)
+# calculate the modularity
+modularity(simple_subgraph, membership = as.vector(communities$membership))
+# Plot the communities
+plot(communities, simple_subgraph,
+     vertex.shape = "circle",
+     vertex.size = V(simple_subgraph)$degree,
+     edge.arrow.size = 0.15,
+     vertex.label.cex = 0.8, # label font size
+     vertex.label.color = "black",
+     vertex.label.family = "Helvetica",
+     vertex.label.font = 2, # bold  font
+)
